5d25ce6f6d6517327693ee3ac533f43b5764f079
David Noguera <david.noguera@techmahindra.com>
2023-01-18 11:24:51 +1100

    Solution to assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,14 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver'
+  userName = '';
+  isUserNameEmpty: boolean;
 
   constructor() {
+    if (this.userName.length > 0) {
+      this.isUserNameEmpty = false;
+    } else {
+      this.isUserNameEmpty = true;
+    }
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onUpdateUserName($event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value
+    if (this.userName.length > 0) {
+      this.isUserNameEmpty = false;
+    } else {
+      this.isUserNameEmpty = true;
+    }
   }
 
   onCreateServer() {
